a5fa86a080fb5e5953df4ba437f88f9b69a6096c
alissonHenSantos <alisson.henriquedns@gmail.com>
2025-10-29 23:41:51 -0300

    feat: estrtura inicial + user

---
+++ user/models.py
@@ -0,0 +1,17 @@
+from sqlalchemy import inspect
+from .. import db 
+
+
+class user(db.Model):
+    id           = db.Column(db.String(50), primary_key=True, nullable=False, unique=True)
+
+    email        = db.Column(db.String(100), nullable=False, unique=True)
+    password     = db.Column(db.String(355), nullable=False)
+    username     = db.Column(db.String(100), nullable=False)
+
+
+    def toDict(self):
+        return { c.key: getattr(self, c.key) for c in inspect(self).mapper.column_attrs }
+
+    def __repr__(self):
+        return "<%r>" % self.email
---
+++ user/urls.py
@@ -0,0 +1,17 @@
+from flask import request
+
+from ..app import app
+from .controllers import list_all_users_controller, create_user_controller, retrieve_user_controller, update_user_controller, delete_user_controller
+
+@app.route("/users", methods=['GET', 'POST'])
+def list_create_users():
+    if request.method == 'GET': return list_all_users_controller()
+    if request.method == 'POST': return create_user_controller()
+    else: return 'Method is Not Allowed'
+
+@app.route("/users/<user_id>", methods=['GET', 'PUT', 'DELETE'])
+def retrieve_update_destroy_users(user_id):
+    if request.method == 'GET': return retrieve_user_controller(user_id)
+    if request.method == 'PUT': return update_user_controller(user_id)
+    if request.method == 'DELETE': return delete_user_controller(user_id)
+    else: return 'Method is Not Allowed'
